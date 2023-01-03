42242e23dd59333ecfba8340afff4f3c0db3cfad
Vedant <vedantnp200@gmail.com>
2023-01-03 14:38:30 +0530

    Initial Commit

---
+++ src/main/java/com/CRM/app/model/Customer.java
@@ -0,0 +1,76 @@
+package com.CRM.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "customer", uniqueConstraints = @UniqueConstraint(columnNames = "email, contact"))
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@Column(name = "First_Name")
+	private String firstName;
+	
+	@Column(name = "Last_Name")
+	private String lastName;
+	
+	private String email;
+	private Long contact;
+	
+	public Customer(String firstName, String lastName, String email, Long contact) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.contact = contact;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getContact() {
+		return contact;
+	}
+
+	public void setContact(Long contact) {
+		this.contact = contact;
+	}
+
+}
